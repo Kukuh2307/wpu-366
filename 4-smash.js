94c7518762ec3ffbb58a6568b8b30f9a3a0a8928
kukuh2307 <kukuhagung12@gmail.com>
2024-01-29 23:38:17 +0700

    4/366

---
+++ 4-smash.js
@@ -0,0 +1,9 @@
+// opsi 1
+// function smash (words) {
+//     return words.join(' ')
+//  };
+
+// opsi 2
+let smash = (words) => words.join(" ");
+
+console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
